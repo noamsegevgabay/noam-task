9f33d9606faa1b80b237f328a879e9f46c849aac
noamsegevgabay <88709178+noamsegevgabay@users.noreply.github.com>
2024-06-02 21:42:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 // Create a simple HTTP server
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, noammmmmm1 :)!\n');
+  res.end('Hello, cloudride :)!\n');
 });
 
 // Listen on port 80
